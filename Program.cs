fa6f67399bf82fef27dd62027dc7986a007a73c2
Aurionpro <Aurionpro@ACER1208>
2023-09-22 20:03:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using FacadePatternApp;
+
+namespace FacadePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HotelReception.GetIndianMenu();
+        }
+    }
+}
